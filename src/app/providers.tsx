4e464c1f847a5b546538e1065c6e0451b976d044
ShuvoFifthElement <sh.das@thefifthelement.co>
2026-07-15 04:47:44 +0600

    store persist gate updated

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,17 +4,20 @@ import { useRef } from "react";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
-import { AppStore, makeStore, store } from "@/store";
+import { AppStore, makeStore } from "@/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const storeRef = useRef<AppStore | null>(null);
+  const persistorRef = useRef<ReturnType<typeof persistStore> | null>(null);
+
   if (!storeRef.current) {
     storeRef.current = makeStore();
+    persistorRef.current = persistStore(storeRef.current);
   }
-  const persistor = persistStore(storeRef.current!);
+
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+    <Provider store={storeRef.current}>
+      <PersistGate loading={null} persistor={persistorRef.current!}>
         {children}
       </PersistGate>
     </Provider>
